2aa7673391dd9137e440b2bff12691b669e0cd09
AndreyCombita <125753783+AndreyCombita@users.noreply.github.com>
2023-08-10 15:05:23 -0500

    Add files via upload

--- Calculadora2.java
+++ Calculadora2.java
@@ -2,53 +2,18 @@
 package calculadora2;
 
 import java.util.Scanner;
-import java.lang.math;
-        
 
 
 public class Calculadora2 {
-    double sum;
-    double res;
-    double mult;
-    double div;
-    double sen;
-    double cos;
-    double tan;
-    double raize;
-    double iva;
+    float num1;
+    float num2;
+    
 
-    @SuppressWarnings("empty-statement")
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-       int opc=0;
-        Scanner in = new Scanner(System.in);
-        for (;;){                 
-            System.out.println("\tCalculadora\n\n"+ "1. Suma \n2. Resta"
-                    + "\n3. Multiplicacion \n4. Division\n5. seno \n6.Coseno \n7.Raiz enesima"
-+ "                 \n8. Raiz enesima \n9.Potencia enecima \n10.IVA");
-            do{System.out.println("Digite una opcion 1-5");
-            opc= in.nextInt();
-            break;
-            
-            }while (opc < 0 || opc>10);
-            switch(opc){
-                case 1: break;
-                case 2: break;
-            }
-            if (opc==1){
-                System.out.println("Digite el primer numero");
-                float a= in.nextInt();
-                System.out.println("Digite el segundo numero");
-                float b= in.nextInt();
-                float sum = a+b;
-                System.out.println("El resultado de la suma es:"+sum);
-                
-                
-            }
-            ;
-            if (opc==5)
-                break;
-            }
-            System.out.println("Finalizado");
+        // TODO code application logic here
     }
     
 }
--- Metodos.java
+++ Metodos.java
@@ -4,10 +4,42 @@
  */
 package calculadora2;
 
-/**
- *
- * @author Andrey Combita
- */
+import java.lang.Math:
+
+
 public class Metodos {
+    double num1;
+    double num2;
+    
+    public Metodos(){
+    this.num1=num1;
+    this.num2=num2 ;
+    }
+    //basicos
+    public void suma (){
+        double suma=this.num1+this.num2;
+    }
+    public void resta (){
+        double resta=this.num1-this.num2;
+    }
+    public void multiplicaion (){
+        double multiplicacion=this.num1*this.num2;
+    }
+    public void division (){
+        double division=this.num1/this.num2;
+    
+    }
+    //operaciones no basicas
+    public void raize(){
+        double raize=Math.pow(num1, 1/num2);}
+    public void pote(){
+        double pote=Math.pow(num1, num2);
+    }
+    public void iva (){
+        double iva= num1*num2;
+        
+    }
+    }
+    
     
 }
