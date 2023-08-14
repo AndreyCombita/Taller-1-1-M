7334cca82deee46a27afa3934123a1eddfd7360e
AndreyCombita <125753783+AndreyCombita@users.noreply.github.com>
2023-08-13 21:56:44 -0500

    Add files via upload

--- Calculadora2.java
+++ Calculadora2.java
@@ -1,19 +1,37 @@
-
 package calculadora2;
 
 import java.util.Scanner;
 
 
 public class Calculadora2 {
-    float num1;
-    float num2;
+    double num1;
+    double num2;
+    
     
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+      int opc=0;
+        Scanner in = new Scanner(System.in);
+        for (;;){                 
+            System.out.println("\tCALCULADORA\n\n"+ "1. Suma\n2.Resta"
+                    + "\n3.Multiplicacion \n4. Division\n5. sen \n6.Coseno \n7. Tangente"
+                    + "\n8. Raiz enesima \n9. Potencia enesima \n10.IVA  \n11.Salir");
+            do{System.out.println("Digite una opcion 1-11");
+            opc= in.nextInt();
+            break;
+            
+            }while (opc < 0 || opc>11);
+            switch(opc){
+                case 1: break;
+                case 2: break;
+            }
+            if (opc==11)
+                break;
+            }
+            System.out.println("Finalizado");
     }
     
 }
--- Metodos.java
+++ Metodos.java
@@ -4,7 +4,7 @@
  */
 package calculadora2;
 
-import java.lang.Math:
+import java.lang.Math;
 
 
 public class Metodos {
@@ -18,28 +18,48 @@ public class Metodos {
     //basicos
     public void suma (){
         double suma=this.num1+this.num2;
+        System.out.println("su resultado es igual a "+ suma);
     }
     public void resta (){
         double resta=this.num1-this.num2;
+        System.out.println("su resultado es igual a "+ resta);
     }
     public void multiplicaion (){
         double multiplicacion=this.num1*this.num2;
+        System.out.println("su resultado es igual a "+ multiplicacion);
     }
     public void division (){
         double division=this.num1/this.num2;
+        System.out.println("su resultado es igual a "+ division);
     
     }
     //operaciones no basicas
     public void raize(){
-        double raize=Math.pow(num1, 1/num2);}
+        double raize=Math.pow(this.num1, 1/this.num2);
+        System.out.println("su resultado es igual a "+ raize);}
     public void pote(){
-        double pote=Math.pow(num1, num2);
-    }
+        double pote=Math.pow(this.num1, this.num2);
+        System.out.println("su resultado es igual a "+ pote);}
+    
     public void iva (){
-        double iva= num1*num2;
+        double iva= this.num1*this.num2;
+        System.out.println("su resultado es igual a "+ iva);}
+        //trigonometricas
+        
+    public void sen(){
+        double sen= Math.sin(this.num1);
+        System.out.println("su resultado es igual a "+ sen);}
+    public void cos(){
+        double cos= Math.cos(this.num1);
+        System.out.println("su resultado es igual a "+ cos);}
+    public void tan(){
+        double tan= Math.tan(this.num1);
+        System.out.println("su resultado es igual a "+ tan);}
+    
+        
         
     }
-    }
     
     
-}
+    
+
